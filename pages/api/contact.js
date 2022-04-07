dba7f041414293be3453d888c63c6c50b4447753
Varga Imre <vargaimre2@nisz.hu>
2022-04-07 13:15:32 +0200

    connect to mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,11 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+import 'dotenv/config'
+
+
+
+
+async function handler(req, res) {
     if(req.method === 'POST') {
         const { email, name, message } = JSON.parse(req.body)
         if(!email || !email.includes('@') || !name || !message || name.trim() == '' || message.trim() == '') {
@@ -12,7 +19,28 @@ function handler(req, res) {
             message
         }
 
-        console.log(newMessage)
+        let client
+
+        try{
+            client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sdqof.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+            
+        } catch (err){
+            res.status(500).json({message: err.message || 'Something went wrong'})
+            return
+        }
+
+        const db = client.db()
+
+        try{
+            const result = await db.collection('messages').insertOne(newMessage)
+            newMessage.id = result.insertedId
+        } catch (err){
+            res.status(500).json({message: err.message})
+            return
+        }
+
+        client.close()
+
 
         res.status(201).json({message: 'Success', message: newMessage})
     }
